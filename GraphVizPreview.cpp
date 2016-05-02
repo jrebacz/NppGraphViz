aa9269aeafc4531bf34e497225236726cfd89c6a
Jeff <jeffrey.rebacz@gmail.com>
2016-05-01 21:18:36 -0500

    Working on #1.  Preview window is now resizable.  The graph may now get scaled down to fix the window, but the graph may not stretch beyond its actual size.

--- GraphVizPreview.cpp
+++ GraphVizPreview.cpp
@@ -106,6 +106,12 @@ INT_PTR CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		getGraphVizPreview()->draw();
 		break;
     }
+    case WM_SIZE:
+        RedrawWindow(getGraphVizPreview()->m_hDlg, NULL, NULL, RDW_INVALIDATE);
+        break;
+    case WM_EXITSIZEMOVE:
+        RedrawWindow(getGraphVizPreview()->m_hDlg, NULL, NULL, RDW_INVALIDATE);
+        break;
 	case WM_COMMAND:
         switch (LOWORD(wParam))
         {
@@ -388,77 +394,111 @@ void GraphVizPreview::graph(bool saveAs)
 
 void GraphVizPreview::draw()
 {
-	if (m_bmp_data.empty())
-		return;
+    if (m_bmp_data.empty())
+        return;
 
-	HWND pImage = GetDlgItem(m_hDlg, ID_PICTURE);
+    HWND pImage = GetDlgItem(m_hDlg, ID_PICTURE);
 
-	{
-		HWND pEdit = GetDlgItem(m_hDlg, IDC_GVP_EDIT);
-		if (m_b_err)
-		{
-			const int offset = 40;
-			SetWindowPos(m_hDlg, NULL, 0, 0, 600, 400, SWP_NOMOVE);
-			SetWindowPos(pEdit, NULL, 0, 0, 600 - offset, 400 - offset, SWP_NOMOVE);
-			m_bmp_data.push_back('\0');
-			SetWindowTextA(pEdit, &m_bmp_data[0]);
-			ShowWindow(pEdit, SW_SHOWNORMAL);
-			EnableWindow(pEdit, TRUE);
+    {
+        HWND pEdit = GetDlgItem(m_hDlg, IDC_GVP_EDIT);
+        if (m_b_err)
+        {
+            const int offset = 40;
+            SetWindowPos(m_hDlg, NULL, 0, 0, 600, 400, SWP_NOMOVE);
+            SetWindowPos(pEdit, NULL, 0, 0, 600 - offset, 400 - offset, SWP_NOMOVE);
+            m_bmp_data.push_back('\0');
+            SetWindowTextA(pEdit, &m_bmp_data[0]);
+            ShowWindow(pEdit, SW_SHOWNORMAL);
+            EnableWindow(pEdit, TRUE);
 
-			ShowWindow(pImage, SW_HIDE);
-			EnableWindow(pImage, FALSE);
+            ShowWindow(pImage, SW_HIDE);
+            EnableWindow(pImage, FALSE);
 
-			InvalidateRect(m_hDlg, NULL, TRUE);
-			return;
-		}
-		else
-		{
-			SetWindowTextA(pEdit, "");
-			ShowWindow(pEdit, SW_HIDE);
-			EnableWindow(pEdit, FALSE);
+            InvalidateRect(m_hDlg, NULL, TRUE);
+            return;
+        }
+        else
+        {
+            SetWindowTextA(pEdit, "");
+            ShowWindow(pEdit, SW_HIDE);
+            EnableWindow(pEdit, FALSE);
 
-			ShowWindow(pImage, SW_SHOWNORMAL);
-			EnableWindow(pImage, TRUE);
-		}
-	}
+            ShowWindow(pImage, SW_SHOWNORMAL);
+            EnableWindow(pImage, TRUE);
+        }
+    }
 
-	HBITMAP hBmp;
-	{
-		char *pBuffer = &*m_bmp_data.begin();
-		tagBITMAPFILEHEADER bfh = *(tagBITMAPFILEHEADER*)pBuffer;
-		tagBITMAPINFOHEADER bih = *(tagBITMAPINFOHEADER*)(pBuffer + sizeof(tagBITMAPFILEHEADER));
-		RGBQUAD             rgb = *(RGBQUAD*)(pBuffer + sizeof(tagBITMAPFILEHEADER) + sizeof(tagBITMAPINFOHEADER));
+    HBITMAP hBmp;
+    {
+        char *pBuffer = &*m_bmp_data.begin();
+        tagBITMAPFILEHEADER bfh = *(tagBITMAPFILEHEADER*)pBuffer;
+        tagBITMAPINFOHEADER bih = *(tagBITMAPINFOHEADER*)(pBuffer + sizeof(tagBITMAPFILEHEADER));
+        RGBQUAD             rgb = *(RGBQUAD*)(pBuffer + sizeof(tagBITMAPFILEHEADER) + sizeof(tagBITMAPINFOHEADER));
 
-		BITMAPINFO bi;
-		bi.bmiColors[0] = rgb;
-		bi.bmiHeader = bih;
+        BITMAPINFO bi;
+        bi.bmiColors[0] = rgb;
+        bi.bmiHeader = bih;
 
-		char* pPixels = (pBuffer + bfh.bfOffBits);
-		char* ppvBits = nullptr;
+        char* pPixels = (pBuffer + bfh.bfOffBits);
+        char* ppvBits = nullptr;
 
-		hBmp = CreateDIBSection(NULL, &bi, DIB_RGB_COLORS, (void**)&ppvBits, NULL, 0);
-		SetDIBits(NULL, hBmp, 0, bih.biHeight, pPixels, &bi, DIB_RGB_COLORS);
-	}
+        hBmp = CreateDIBSection(NULL, &bi, DIB_RGB_COLORS, (void**)&ppvBits, NULL, 0);
+        SetDIBits(NULL, hBmp, 0, bih.biHeight, pPixels, &bi, DIB_RGB_COLORS);
+    }
 
-	BITMAP 			bitmap;
-	GetObject(hBmp, sizeof(bitmap), &bitmap);
-	// Resize
-    SetWindowPos(m_hDlg, NULL, 0, 0, bitmap.bmWidth + 40, bitmap.bmHeight + 60, SWP_NOMOVE | SWP_NOACTIVATE);
-    SetWindowPos(pImage, NULL, 0, 0, bitmap.bmWidth + 40, bitmap.bmHeight + 60, SWP_NOMOVE | SWP_NOACTIVATE);
+    BITMAP 			bitmap;
+    GetObject(hBmp, sizeof(bitmap), &bitmap);
+    // Resize
+    RECT dlg_dimensions, output_dimensions;
+    GetClientRect(m_hDlg, &dlg_dimensions);
 
+    // Maximize the draw area to the available area.
+    SetWindowPos(pImage, NULL, 0, 0, dlg_dimensions.right, dlg_dimensions.bottom, SWP_NOMOVE | SWP_NOACTIVATE);
+
+    // We will have to scale the bitmap, this section applies the scaling factor
+    output_dimensions = dlg_dimensions;
+    output_dimensions.right -= 1;
+    output_dimensions.bottom -= 1;
+
+    double width_ratio = double(output_dimensions.right) / bitmap.bmWidth;
+    double height_ratio = double(output_dimensions.bottom) / bitmap.bmHeight;
+
+    if (width_ratio < 1.0 || height_ratio < 1.0)
+    {
+        if (width_ratio < height_ratio)
+        {
+            output_dimensions.right = bitmap.bmWidth * width_ratio;
+            output_dimensions.bottom = bitmap.bmHeight * width_ratio;
+        }
+        else
+        {
+            output_dimensions.right = bitmap.bmWidth * height_ratio;
+            output_dimensions.bottom = bitmap.bmHeight * height_ratio;
+        }
+    }
+    else
+    {
+        output_dimensions.right = bitmap.bmWidth;
+        output_dimensions.bottom = bitmap.bmHeight;
+    }
+
+    // Begin painting    
 	PAINTSTRUCT 	ps;
 	HDC 			hdc = BeginPaint(pImage, &ps);
 	HDC hdcMem = CreateCompatibleDC(hdc);
 	HGDIOBJ oldBitmap = SelectObject(hdcMem, hBmp);
 
-	if (false)
-	{
-		BitBlt(hdc, 0, 0, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCINVERT);
-	}
-	else
-	{
-		BitBlt(hdc, 0, 0, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
-	}
+    // Draw dark grey everywhere
+    HBRUSH bg_color = CreateSolidBrush(RGB(200,200,200));
+    FillRect(hdc, &dlg_dimensions, bg_color);
+    DeleteObject(bg_color);
+
+    // Copy from bitmap to dialog rectangle, with scaling.
+    StretchBlt(hdc, 
+        0, 0, output_dimensions.right, output_dimensions.bottom,
+        hdcMem,
+        0, 0, bitmap.bmWidth, bitmap.bmHeight, SRCCOPY);
+
 
 	SelectObject(hdcMem, oldBitmap);
 
